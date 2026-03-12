3656718593d6c152c461a846d24accec92e9bb0d
Bosssmann810 <sealered@gmail.com>
2026-03-12 11:21:40 -0300

    BEgan working on adding more sounds effects to the player, they are a bit off

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -7,6 +7,7 @@ public class Shoot : MonoBehaviour
     [SerializeField] private Animator playerAnimator;
     public int shoothash = Animator.StringToHash("Shoot");
     private bool shooting = false;
+    public PlayerSoundManager SoundManager;
     
     void Start()
     {
@@ -21,11 +22,13 @@ public class Shoot : MonoBehaviour
             Debug.Log("pewpew");
             shooting = true;
             _laser.SetActive(true);
+            SoundManager.LaserAudioOn();
         }
         if (Input.GetKeyUp(KeyCode.T))
         {
             shooting = false;
             _laser.SetActive(false);
+            SoundManager.LaserAudioOff();
         }
         HandleShootingAnimation();
     }
